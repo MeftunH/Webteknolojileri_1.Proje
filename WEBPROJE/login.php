43af1f87b7a5fc76c98a170f8aab4638b61c85d4
Meftun Hesimli <48466124+MeftunH@users.noreply.github.com>
2020-05-09 17:06:20 +0300

    yorum satirlari

--- WEBPROJE/login.php
+++ WEBPROJE/login.php
@@ -5,15 +5,16 @@ if(empty($_POST)==false)
    
     $kadi=$_POST["kadi"];
     $sifre=$_POST["sifre"];
-    
+    //kontrol
        if($kadi=="g181210554@sakarya.edu.tr"&& $sifre=="123") 
        {
+         //dogruysa
            echo "Hosgeldin g181210554@sakarya.edu.tr";
            $girisdogrumu=true;
        }
     else
     {
-    
+    //degilse logine geri don
         header ("Location:Login.html");
 
     }
